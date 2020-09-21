46bcfc53ffba329a12ce541c06e7282feead2d43
diplomatic-link-4183 <noe.reyes92@yahoo.com>
2020-09-21 17:41:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 function add(a, b) {
   return a + b;
 }
-function subtract()
-function multiply()
-function divide()
+function subtract(a, b) {
+  return a - b;
+}
+function multiply(a, b) {
+  return a * b;
+}
+function divide(a, b) {
+  return a / b;
+}
